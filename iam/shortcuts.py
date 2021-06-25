bef14c2384e72477b8a596530add7a17870b9643
homholueng <homholueng@gmail.com>
2021-06-25 14:51:52 +0800

    feature: iam.shortcuts.allow_or_raise_auth_failed 支持使用 cache 判断

--- iam/shortcuts.py
+++ iam/shortcuts.py
@@ -10,15 +10,14 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-
 from . import Request
 from .exceptions import AuthFailedException
 
 
-def allow_or_raise_auth_failed(iam, system, subject, action, resources, environment=None):
+def allow_or_raise_auth_failed(iam, system, subject, action, resources, environment=None, cache=False):
     request = Request(system, subject, action, resources, environment)
 
-    allowed = iam.is_allowed(request)
+    allowed = iam.is_allowed_with_cache(request) if cache else iam.is_allowed(request)
 
     if not allowed:
         raise AuthFailedException(system, subject, action, resources)
