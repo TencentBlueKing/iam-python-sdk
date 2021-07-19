a2ab2100619f7456bdb2f72de9edf737100f7bf9
wklken <wklken@gmail.com>
2021-07-19 17:17:36 +0800

    update license (#33)

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 """
+TencentBlueKing is pleased to support the open source community by making
+蓝鲸智云-权限中心Python SDK(iam-python-sdk) available.
+Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+
+# description
 should support both python2 and python3
 
 表达式求值 demo
@@ -10,8 +20,8 @@ DONE:
 3. 实现递归求值
 4. 加入模板方法限制, 确保每个operator实现eval
 5. 加入Enum类, 去除字面量
-
 """
+
 from __future__ import print_function
 
 import logging
