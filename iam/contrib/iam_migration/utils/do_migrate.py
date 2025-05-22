da492ac715f2c4346ef72d6230034adc437006a2
nannan00 <17491932+nannan00@users.noreply.github.com>
2025-05-22 15:06:15 +0800

    feat: disable urllib3 warn

--- iam/contrib/iam_migration/utils/do_migrate.py
+++ iam/contrib/iam_migration/utils/do_migrate.py
@@ -18,10 +18,13 @@ import json
 import os
 
 import requests
+import urllib3
 
 # NOTE: the usage doc https://bk.tencent.com/docs/document/6.0/160/8388
 
 __version__ = "1.0.0"
+# no more useless warning
+urllib3.disable_warnings()
 
 BK_APIGATEWAY_URL = os.getenv("BK_IAM_APIGATEWAY_URL", "https://bkapi.example.com/api/bk-iam/prod/")
 
